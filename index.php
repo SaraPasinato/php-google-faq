3e6bbb528cb0b4cd80d8ddda24331591e9c0b787
SaraPasinato <pasinatosara.sp@gmail.com>
2021-09-22 18:10:15 +0200

    add footer + header

--- index.php
+++ index.php
@@ -14,19 +14,19 @@ $questions = [
         "description" => "<p>Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.</p><p>Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.</p><p>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href=\"https://www.google.com/safetycenter/\">Centro Google per la sicurezza online</a>.</p><p><a href=\"https://privacy.google.com\">Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.</p>",
     ],
     [
-        "title"=>"Perché il mio account è associato a un paese?",
-        "description"=> "<p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p><ol><li class=\"smANld\">La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti: <ol type=\"a\"><li class=\"smANld\">Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li><li class=\"smANld\">Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li></ol></li><li class=\"smANld\">La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li></ol><p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>",
-        "subtitle"=>"Stabilire il paese associato al tuo account",
+        "title" => "Perché il mio account è associato a un paese?",
+        "description" => "<p>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:</p><ol><li class=\"smANld\">La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti: <ol type=\"a\"><li class=\"smANld\">Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li><li class=\"smANld\">Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li></ol></li><li class=\"smANld\">La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li></ol><p>Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.</p>",
+        "subtitle" => "Stabilire il paese associato al tuo account",
         "sub_description" => "<p>Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.</p><p>I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.</p><p>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. <a href=\"country-association-contact-us\" class=\"N6CPUe\" target=\"_blank\">Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.</p>"
 
     ],
     [
-        'title'=> "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
-        'description'=> "<p>I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href=\"https://support.google.com/websearch/answer/9109?hl=it\">contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href=\"https://support.google.com/legal/contact/lr_eudpa?product=websearch&amp;hl=it\">fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href=\"https://support.google.com/websearch/troubleshooter/3111061?hl=it\">visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.</p>",
+        'title' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        'description' => "<p>I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href=\"https://support.google.com/websearch/answer/9109?hl=it\">contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a href=\"https://support.google.com/legal/contact/lr_eudpa?product=websearch&amp;hl=it\">fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href=\"https://support.google.com/websearch/troubleshooter/3111061?hl=it\">visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>.</p>",
     ],
     [
-        'title'=>"Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
-        'description'=>"<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href=\"privacy/key-terms#toc-terms-http\">URL referrer</a>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href=\"https://support.google.com/websearch/answer/173733\">qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>",
+        'title' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+        'description' => "<p>In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href=\"privacy/key-terms#toc-terms-http\">URL referrer</a>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href=\"https://support.google.com/websearch/answer/173733\">qui</a>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.</p>",
     ],
 ];
 
@@ -42,36 +42,142 @@ $questions = [
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,500&display=swap" rel="stylesheet">
+    <!-- font-awsome -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css' integrity='sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==' crossorigin='anonymous' />
     <link rel="stylesheet" href="css/style.css">
     <title>Google-Faq</title>
 </head>
 
 <body>
-    <!--//TODO: header style-->
-    <header></header>
-    <main class="container">
+    <header>
+        <div id="top-header" class="container-lg">
+            <div class="row">
+                <div class="logos ms-2">
+                    <div class="row">
+                        <div class="col">
+                            <img class="" src="./img/googlelogo_clr_74x24px.svg" alt="google logo">
+                            <a id="privacy" href="#">Privacy e termini</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="btn-controls col-3">
+                    <div class="row">
+                        <svg class="mb-1" focusable="false" viewBox="0 0 20 20">
+                            <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                        </svg>
+                        <a class="ms-1 me-1 btn rounded"><i class="fas fa-signature"></i></a>
+                    </div>
+                </div>
+            </div>
+            <nav id="nav-links">
+                <ul class="row">
+                    <li><a href="#">Introduzione</a></li>
+                    <li><a href="#">norme sulla privacy</a></li>
+                    <li><a href="#">termini di servizio</a></li>
+                    <li><a href="#">Tecnologie</a></li>
+                    <li><a href="#" class="active">Domande frequenti</a></li>
+                </ul>
+            </nav>
+    </header>
+    <main class="container mt-12">
         <?php foreach ($questions as $question => $item) { ?>
-            <section id="section-q-<?= $question ?>"class="mt-4">
+            <section id="section-q-<?= $question ?>" class="mt-4">
                 <h2><?php echo $item['title'] ?></h2>
                 <div><?php echo $item['description'] ?></div>
                 <h3>
                     <?php
-                     $subtitle= (!empty($item['subtitle'])) ? $item['subtitle']: '';
-                     echo $subtitle
-                     ?>
+                    $subtitle = (!empty($item['subtitle'])) ? $item['subtitle'] : '';
+                    echo $subtitle
+                    ?>
                 </h3>
                 <div>
                     <?php
-                     $desc= (!empty($item['sub_description'])) ? $item['sub_description']: '';
-                     echo $desc
-                     ?>
+                    $desc = (!empty($item['sub_description'])) ? $item['sub_description'] : '';
+                    echo $desc
+                    ?>
                 </div>
-                
+
             </section>
         <?php } ?>
     </main>
-    <!--//TODO: footer style-->
-    <footer></footer>
+    <footer id="main-footer">
+        <div class="row container" >
+            <ul class="row">
+                <li>google</li>
+                <li>links</li>
+                <li>terms</li>
+                <li>copyright</li>
+            </ul>
+            <div class="item">
+            <i class="fas fa-globe"></i>
+            <select id="lang-select">
+                <option value="af">Afrikaans</option>
+                <option value="id">Bahasa Indonesia</option>
+                <option value="ms">Bahasa Melayu</option>
+                <option value="ca">Català</option>
+                <option value="cs">Čeština</option>
+                <option value="da">Dansk</option>
+                <option value="de">Deutsch</option>
+                <option value="et">Eesti</option>
+                <option value="en">English</option>
+                <option value="en-GB">English (United Kingdom)</option>
+                <option value="es">Español</option>
+                <option value="es-419">Español (Latinoamérica)</option>
+                <option value="eu">Euskara</option>
+                <option value="fil">Filipino</option>
+                <option value="fr">Français</option>
+                <option value="fr-CA">Français (Canada)</option>
+                <option value="gl">Galego</option>
+                <option value="hr">Hrvatski</option>
+                <option value="zu">Isizulu</option>
+                <option value="is">Íslenska</option>
+                <option selected="" value="it">Italiano</option>
+                <option value="sw">Kiswahili</option>
+                <option value="lv">Latviešu</option>
+                <option value="lt">Lietuvių</option>
+                <option value="hu">Magyar</option>
+                <option value="nl">Nederlands</option>
+                <option value="no">Norsk</option>
+                <option value="pl">Polski</option>
+                <option value="pt-BR">Português (Brasil)</option>
+                <option value="pt-PT">Português (Portugal)</option>
+                <option value="ro">Română</option>
+                <option value="sk">Slovenčina</option>
+                <option value="sl">Slovenščina</option>
+                <option value="sr-Latn">Srpski</option>
+                <option value="fi">Suomi</option>
+                <option value="sv">Svenska</option>
+                <option value="vi">Tiếng Việt</option>
+                <option value="tr">Türkçe</option>
+                <option value="el">Ελληνικά</option>
+                <option value="bg">Български</option>
+                <option value="ru">Русский</option>
+                <option value="sr">Српски</option>
+                <option value="uk">Українська</option>
+                <option value="iw">‫עברית‬</option>
+                <option value="ur">‫اردو‬</option>
+                <option value="ar">‫العربية‬</option>
+                <option value="fa">‫فارسی‬</option>
+                <option value="am">አማርኛ</option>
+                <option value="mr">मराठी</option>
+                <option value="hi">हिन्दी</option>
+                <option value="bn">বাংলা</option>
+                <option value="gu">ગુજરાતી</option>
+                <option value="ta">தமிழ்</option>
+                <option value="te">తెలుగు</option>
+                <option value="kn">ಕನ್ನಡ</option>
+                <option value="ml">മലയാളം</option>
+                <option value="th">ไทย</option>
+                <option value="ko">한국어</option>
+                <option value="zh-HK">中文 (香港)</option>
+                <option value="zh-CN">中文（简体中文）</option>
+                <option value="zh-TW">中文（繁體中文）</option>
+                <option value="ja">日本語</option>
+            </select>
+            </div>
+ 
+        </div>
+    </footer>
 </body>
 
 </html>
